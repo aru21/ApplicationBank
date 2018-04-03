bbcc6942ec4c379b9259ce3814b8c828ccac70ba
Ace <Ace@DESKTOP-VU1NAKB>
2018-04-03 19:48:08 +0530

    sop added in main

--- src/main/java/com/my/demo/AnkitApplication.java
+++ src/main/java/com/my/demo/AnkitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnkitApplication {
 
 	public static void main(String[] args) {
+		System.out.println("parosh");
 		SpringApplication.run(AnkitApplication.class, args);
 	}
 }
